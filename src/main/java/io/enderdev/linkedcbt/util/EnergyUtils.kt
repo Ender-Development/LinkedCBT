144094b118558677b91ea170d1cbc41348c4db77
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2026-01-11 11:44:26 +0100

    I might be stupid

--- src/main/java/io/enderdev/linkedcbt/util/EnergyUtils.kt
+++ src/main/java/io/enderdev/linkedcbt/util/EnergyUtils.kt
@@ -4,18 +4,18 @@ import net.minecraftforge.energy.IEnergyStorage
 
 object EnergyUtils {
 	class ReceiveOnlyWrapper(val wrapped: IEnergyStorage) : IEnergyStorage by wrapped {
-		override fun canReceive() =
-			false
-
-		override fun receiveEnergy(maxReceive: Int, simulate: Boolean) =
-			0
-	}
-
-	class ExtractOnlyWrapper(val wrapped: IEnergyStorage) : IEnergyStorage by wrapped {
 		override fun canExtract() =
 			false
 
 		override fun extractEnergy(maxReceive: Int, simulate: Boolean) =
+			0
+	}
+
+	class ExtractOnlyWrapper(val wrapped: IEnergyStorage) : IEnergyStorage by wrapped {
+		override fun canReceive() =
+			false
+
+		override fun receiveEnergy(maxReceive: Int, simulate: Boolean) =
 			0
 	}
 }
